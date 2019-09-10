1d9728abd1a1985d6bbc01c0e7557d4747f0a3ff
Leszek Kurzyna <v-lekurz@microsoft.com>
2019-09-09 18:29:07 -0700

    fixed hana install script for 51053787 version

--- scripts/allvmsizes.sh
+++ scripts/allvmsizes.sh
@@ -74,7 +74,7 @@ echo $Uri >> /tmp/url.txt
 
 cp -f /etc/waagent.conf /etc/waagent.conf.orig
 sedcmd="s/ResourceDisk.EnableSwap=y/ResourceDisk.EnableSwap=y/g"
-sedcmd2="s/ResourceDisk.SwapSizeMB=0/ResourceDisk.SwapSizeMB=28672/g"
+sedcmd2="s/ResourceDisk.SwapSizeMB=28672/ResourceDisk.SwapSizeMB=28672/g"
 cat /etc/waagent.conf | sed $sedcmd | sed $sedcmd2 > /etc/waagent.conf.new
 cp -f /etc/waagent.conf.new /etc/waagent.conf
 
@@ -342,8 +342,8 @@ SAPBITSDIR="/hana/data/sapbits"
 
 if [ "${hanapackage}" = "51053787" ]
 then 
-  /usr/bin/wget --quiet $Uri/SapBits/${hanapackage}.ZIP
   cd $SAPBITSDIR
+  /usr/bin/wget --quiet $Uri/SapBits/${hanapackage}.ZIP
   mkdir ${hanapackage}
   cd ${hanapackage}
   unzip ../${hanapackage}.ZIP
